28ad3fd83c6f14d6dc0d28447303b31eccbc7b59
MACBOOK PRO <macbookpro@Adeboyes-MacBook-Pro.local>
2026-07-27 14:15:43 +0100

    Day 1: hello.js - the journey begins

---
+++ hello.js
@@ -0,0 +1,15 @@
+//Who I am
+const myName = "Adeboye";
+
+//Where I'm going
+const myGoal = "Backend JavaScript Developer";
+
+//My deadline
+const timeline = "4 months";
+
+console.log("Name:", myName);
+console.log("Goal:", myGoal);
+console.log("Timeline:", timeline);
+console.log("Start date:", new Date().toDateString());
+console.log("Let's get to work.");
+
